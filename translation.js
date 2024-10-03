35ecaf519e331cc2ddb80b1cb2e332782a143cde
Drigrow <86180202+Drigrow@users.noreply.github.com>
2024-10-03 23:44:14 +0800

    Update translation.js

--- translation.js
+++ translation.js
@@ -35,6 +35,7 @@ export default {
         <html lang="en">
         <head>
             <meta charset="UTF-8">
+            <link rel="icon" href="https://drigrowpersonal.eu.org/favicon.ico" type="image/x-icon">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Multilanguage Translation</title>
             <style>
